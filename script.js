82e86cfd46164df6d2e0008434429f3e6326befc
Bruno Ferreira Barbosa <bruno_wfb@hotmail.com>
2025-03-25 17:35:53 -0300

    caluculadora IMC finalizada

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
 function calcularIMC() {
-    var altura = (document.getElementById("altura").value)/100
-    var peso = document.getElementById("peso").value
+    var altura = parseFloat(document.getElementById("altura").value) / 100;
+    var peso = parseFloat(document.getElementById("peso").value);
+    var res = document.getElementById("res");
+
+    var imc = peso / (altura ** 2);
     
-   
-    var imc = peso / altura ** 2;
-    if(imc<18.5){
-        window.alert(" Você está abaixo do peso")
-    } else if(imc<24.9){
-        window.alert(" Você está com peso normal")
-    } else if(imc<29.9){
-        window.alert("Você está com sobrepeso")
-    } else if(imc<39.9){
-        window.alert("Você está com obesidade")
-    } else if(imc>39.9) {
-        window.alert("Você está com obesidade mórbida")
+    let classificacao = '';
+
+    if (imc < 18.5) {
+        classificacao = 'Você está abaixo do peso'
+    } else if (imc < 24.9) {
+        classificacao = 'Você está com peso normal'
+    } else if (imc < 29.9) {
+        classificacao = 'Você está com sobrepeso'
+    } else if (imc < 39.9) {
+        classificacao = 'Você está com obesidade'
+    } else {
+        classificacao = 'Você está com obesidade mórbida'
     }
+    res.innerHTML = `<p class="imc-valor">IMC: ${imc.toFixed(2)}</p><p class="classificacap">(${classificacao})</p>`;
 
 }
 
